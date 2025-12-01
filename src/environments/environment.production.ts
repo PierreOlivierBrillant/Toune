a4acc0e62cec4aa4be664c89205d473ae6fa8feb
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2025-12-01 09:50:01 -0500

    fix: Update serverUrl in production environment configuration

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    serverUrl: 'https://votre-backend-url.com', // À remplacer par l'URL de votre backend en production
+    serverUrl: 'http://llbinfd000700:5003',
     spotifyApiUrl: 'https://accounts.spotify.com',
     clientId: '811a955e1aac4d7ba82affed9f34ce24',
 };
