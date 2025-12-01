e00036cc2f1d49506268f69f328efeb67f7b75a7
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2025-12-01 13:28:34 -0500

    fix: Update serverUrl to use HTTPS for production environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://llbinfd000700:5003',
+    serverUrl: 'https://toune-d7bkcse5f6hpfyg4.canadaeast-01.azurewebsites.net',
     spotifyApiUrl: 'https://accounts.spotify.com',
     clientId: '811a955e1aac4d7ba82affed9f34ce24',
 };
